aaaa0b7b6da987ebcb574b9f5fbe5dbe832ee197
daolao <dalao1002@gmail.com>
2018-12-16 12:13:47 +0100

    fix bug

--- dtc.py
+++ dtc.py
@@ -1,13 +1,11 @@
-from math import inf
 import numpy as np
-import pandas as pd
-from scipy import stats
 
 
 def entropy(observations):
     occurrences = np.bincount(observations)
     probability_mass = occurrences / np.sum(occurrences)
-    return stats.entropy(probability_mass)
+    entropies = -probability_mass * (1-probability_mass)
+    return numpy.nansum(entropies)
 
 
 def gini(observations):
@@ -30,7 +28,7 @@ class DecisionTreeClassifier:
         best_classes = None
         best_subset_X = None
         best_subset_y = None
-        best_G = inf
+        best_G = np.inf
         best_atribute = None
         best_atribute_index = None
 
@@ -71,6 +69,7 @@ class DecisionTreeClassifier:
             for index, c in enumerate(best_classes):
                 best_split[c] = DecisionTreeClassifier.split(
                     best_subset_X[index], best_subset_y[index], atributes, used, evaluate)
+            used[best_atribute] = False
             return {
                 "atribute": best_atribute,
                 "atribute_index": best_atribute_index,
@@ -88,50 +87,10 @@ class DecisionTreeClassifier:
         def classify(x):
             current = self.root
             while not current["terminated"]:
-                current = current["split"][x[current["atribute_index"]]]
+                atribute_index = current["atribute_index"]
+                clazz = x[atribute_index]
+                current = current["split"][clazz]
             else:
                 return current["class"]
 
         return np.apply_along_axis(classify, 1, X)
-
-
-def main():
-    atributes = ["Moroso", "Antiguedad", "Ingreso", "Trabajo fijo"]
-    X = np.array([
-        [1, 2, 1, 1],
-        [0, 0, 1, 1],
-        [1, 1, 2, 1],
-        [0, 2, 2, 0],
-        [0, 0, 2, 1],
-        [1, 1, 1, 1],
-        [0, 1, 2, 1],
-        [0, 0, 0, 1],
-        [0, 2, 1, 0],
-        [1, 1, 0, 0]
-    ])
-    y = np.array([0, 1, 0, 1, 1, 0, 1, 0, 0, 0])
-    model = DecisionTreeClassifier(atributes, criterion=entropy)
-    model.train(X, y)
-    print(model.predict(np.array([[1, 2, 1, 1]])))
-
-    df = pd.read_csv('heart.csv')
-    df["age"] = pd.qcut(df["age"], 5, labels=False)
-    df["trestbps"] = pd.qcut(df["trestbps"], 5, labels=False)
-    df["chol"] = pd.qcut(df["chol"], 5, labels=False)
-    df["thalach"] = pd.qcut(df["thalach"], 5, labels=False)
-    df["oldpeak"] = pd.qcut(df["oldpeak"], 3, labels=False)
-    X = df.values[:, :-1]
-    y = df.values[:, -1]
-    atributes = list(df.columns.values)[:-1]
-    model = DecisionTreeClassifier(atributes, criterion=gini)
-    model.train(X, y)
-    print(np.where(model.predict(X) == y)[0].size / y.size)
-
-    from sklearn import tree
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(X, y)
-    print(np.where(clf.predict(X) == y)[0].size / y.size)
-
-
-if __name__ == "__main__":
-    main()
