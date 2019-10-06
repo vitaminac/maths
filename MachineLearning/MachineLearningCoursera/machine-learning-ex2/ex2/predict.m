399b6d4b72f88cc4b48ae7176961adec42e69fc4
dalao <dalao1002@gmail.com>
2019-10-06 15:06:46 +0200

    Programming Assignment: Logistic Regression

--- MachineLearning/MachineLearningCoursera/machine-learning-ex2/ex2/predict.m
+++ MachineLearning/MachineLearningCoursera/machine-learning-ex2/ex2/predict.m
@@ -16,7 +16,12 @@ p = zeros(m, 1);
 %
 
 
+m = size(X, 1); % Number of training examples
 
+% You need to return the following variables correctly
+p = zeros(m, 1);
+
+p = sigmoid(X * theta) >= 0.5;
 
 
 
